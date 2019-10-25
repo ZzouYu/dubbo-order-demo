41a27ce96ee536fe884680e839fa2c687f78807d
邹宇 <zouyu@tellhow.com>
2019-10-25 11:13:49 +0800

    first

---
+++ dubbo-user/user-provider/src/main/java/com/zy/dubbo/user/App.java
@@ -0,0 +1,15 @@
+package com.zy.dubbo.user;
+
+import sun.applet.Main;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        Main.main(args);
+    }
+}
